d0bc659dcd53f55def23025d7f07b14de56c69ba
Divyanshu050303 <divyanshusingh7060@gmail.com>
2022-06-28 08:54:12 +0530

    candy

--- src/candy.java
+++ src/candy.java
@@ -1,9 +1,25 @@
 import java.util.Scanner;
 
 public class candy {
+    static Scanner sc=new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
         int number= sc.nextInt();
+        for(int i=1;i<=number;++i){
+            solve(i);
+        }
 
-    }
+        }
+        static void solve(int number){
+            for (int i = 0; i <number ; i++) {
+                int n= sc.nextInt();
+                int m= sc.nextInt();
+                int sum=0;
+                for (int j = 0; j < n; j++) {
+                    sum+=sc.nextInt();
+                }
+                System.out.print("Case #" + (i+1) + ": "+ sum%m);
+            }
+        }
+
 }
+
