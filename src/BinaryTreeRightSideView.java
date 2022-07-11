10f84b574ac75d7c780a85155ef15c36b893b122
Divyanshu050303 <divyanshusingh7060@gmail.com>
2022-07-11 12:41:22 +0530

    Binary Tree Right Side View

---
+++ src/BinaryTreeRightSideView.java
@@ -0,0 +1,24 @@
+import javax.swing.tree.TreeNode;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreeRightSideView {
+    //https://leetcode.com/problems/binary-tree-right-side-view/
+//    public List<Integer> rightSideView(TreeNode root) {
+//
+//        List<Integer> list=new ArrayList<>();
+//        rightView(root, list, 0);
+//        return list;
+//
+//    }
+//    void rightView(TreeNode root, List<Integer> arr, int c){
+//        if(root==null){
+//            return;
+//        }
+//        if(c==arr.size())
+//            arr.add(root.val);
+//        rightView(root.right, arr, c+1);
+//        rightView(root.left, arr, c+1);
+//
+//    }
+}
