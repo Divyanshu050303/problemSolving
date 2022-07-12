5115f5c373e6dcb5a17f09febbf6cf291af9d6db
Divyanshu050303 <divyanshusingh7060@gmail.com>
2022-07-12 13:18:06 +0530

    Single Number

---
+++ src/SingleNumber.java
@@ -0,0 +1,11 @@
+public class SingleNumber {
+    //https://leetcode.com/problems/single-number/
+    public int singleNumber(int[] nums) {
+        int xor=0;
+        for(Integer i :nums){
+            xor=xor^i;
+        }
+        return xor;
+
+    }
+}
