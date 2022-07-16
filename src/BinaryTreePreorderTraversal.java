1b6d62c61efd57e9c97ab128e941cd9ce4f4ba9c
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2022-07-16 22:15:05 +0530

    BinaryTreePreorderTraversal

---
+++ src/BinaryTreePreorderTraversal.java
@@ -0,0 +1,16 @@
+public class BinaryTreePreorderTraversal {
+//https://leetcode.com/problems/binary-tree-preorder-traversal/
+
+//    List<Integer> list1=new ArrayList<>();
+//    public List<Integer> preorderTraversal(TreeNode root) {
+//
+//        if(root==null){
+//            return list1;
+//        }
+//        list1.add(root.val);
+//        preorderTraversal(root.left);
+//        preorderTraversal(root.right);
+//        return list1;
+//
+//    }
+}
