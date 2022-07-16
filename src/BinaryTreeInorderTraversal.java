d220bfb0c292b34ff40ad25494935d73730eddd1
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2022-07-16 22:14:43 +0530

    BinaryTreeInorderTraversal

---
+++ src/BinaryTreeInorderTraversal.java
@@ -0,0 +1,20 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreeInorderTraversal {
+    //https://leetcode.com/problems/binary-tree-inorder-traversal/
+//    List<Integer> list1=new ArrayList<>();
+//    public List<Integer> inorderTraversal(TreeNode root) {
+//
+//        if(root==null){
+//
+//            return list1;
+//        }
+//        inorderTraversal(root.left);
+//        list1.add(root.val);
+//
+//        inorderTraversal(root.right);
+//        return list1;
+//    }
+}
