16b5ab14cf0edad0e469c0dd62d3fe3549d65ced
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2022-07-16 22:14:53 +0530

    BinaryTreePostorderTraversal

---
+++ src/BinaryTreePostorderTraversal.java
@@ -0,0 +1,13 @@
+public class BinaryTreePostorderTraversal {
+    //https://leetcode.com/problems/binary-tree-postorder-traversal/
+//    List<Integer> list1=new ArrayList<>();
+//    public List<Integer> postorderTraversal(TreeNode root) {
+//        if(root==null){
+//            return list1;
+//        }
+//        postorderTraversal(root.left);
+//        postorderTraversal(root.right);
+//        list1.add(root.val);
+//        return list1;
+//    }
+}
