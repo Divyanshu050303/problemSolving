6f5b50b6b7c3cbe5de9a66a8866296b1d9a31856
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2022-07-20 10:37:55 +0530

    class

--- src/reverseBits.java
+++ src/reverseBits.java
@@ -2,14 +2,28 @@ import java.util.Scanner;
 
 public class reverseBits {
     //https://leetcode.com/problems/reverse-bits/
-    public int reverseBit(int n) {
-        int  result = 0;
-        for(int k=0; k<=31; k++) {
-            if((n &(1<<k)) !=0) {
-                result = result| 1<<(31-k);
-            }
+//    public int reverseBit(int n) {
+//        int  result = 0;
+//        for(int k=0; k<=31; k++) {
+//            if((n &(1<<k)) !=0) {
+//                result = result| 1<<(31-k);
+//            }
+//        }
+//        return result;
+//    }
+    public static void main(String[] args) {
+        try{
+            Float f1=new Float("3.0");
+            int x=f1.intValue();
+            byte x1=f1.byteValue();
+            double x2=f1.doubleValue();
+            System.out.println(x+x1+x2);
+        }catch (NumberFormatException e){
+            System.out.println("Bad number");
         }
-        return result;
     }
+    /*asdhfaksjd**** */
+    /*ashdfajkshfd//asdfasd*/
+    ///
 
 }
