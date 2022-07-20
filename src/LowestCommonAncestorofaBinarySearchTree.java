14da55dab59fa5ed0a1796da052b8a237aa481c6
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2022-07-20 11:37:17 +0530

    LowestCommonAncestorofaBinarySearchTree

---
+++ src/LowestCommonAncestorofaBinarySearchTree.java
@@ -0,0 +1,16 @@
+public class LowestCommonAncestorofaBinarySearchTree {
+    //https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+//    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//        if(root==null || root==p ||root==q){
+//            return root;
+//        }
+//        TreeNode leftChild=lowestCommonAncestor(root.left, p, q);
+//        TreeNode rightChild=lowestCommonAncestor(root.right, p, q);
+//        if(leftChild==null)
+//            return rightChild;
+//        else if(rightChild==null)
+//            return leftChild;
+//        else return root;
+//
+//    }
+}
