2392b450227007c2c4797ccc7c98588514244224
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2022-07-20 11:37:04 +0530

    BinarySearchTreetoGreaterSumTree

---
+++ src/BinarySearchTreetoGreaterSumTree.java
@@ -0,0 +1,14 @@
+public class BinarySearchTreetoGreaterSumTree {
+    //https://leetcode.com/problems/binary-search-tree-to-greater-sum-tree/
+//    int sum=0;
+//    public TreeNode bstToGst(TreeNode root) {
+//
+//        if(root==null)
+//            return null;
+//        bstToGst(root.right);
+//        sum+=root.val;
+//        root.val=sum;
+//        bstToGst(root.left);
+//        return root;
+//    }
+}
