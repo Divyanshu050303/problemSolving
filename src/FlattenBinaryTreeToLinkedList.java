b5e1f60503efb538318ea4a88a504763b881e2a3
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2022-07-21 10:57:45 +0530

    Flatten Binary Tree To Linked List

---
+++ src/FlattenBinaryTreeToLinkedList.java
@@ -0,0 +1,14 @@
+public class FlattenBinaryTreeToLinkedList {
+    //https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
+//    TreeNode tail=null;
+//    public void flatten(TreeNode root) {
+//        if(root==null)
+//            return;
+//        flatten(root.right);
+//        flatten(root.left);
+//        root.right=tail;
+//        root.left=null;
+//        tail=root;
+//
+//    }
+}
