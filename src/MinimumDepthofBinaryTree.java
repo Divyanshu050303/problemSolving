dac685e39b884608aca2b0b4b26f00bff4f383db
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2022-07-21 10:58:09 +0530

    Minimum Depth of Binary Tree

---
+++ src/MinimumDepthofBinaryTree.java
@@ -0,0 +1,11 @@
+public class MinimumDepthofBinaryTree {
+    //https://leetcode.com/problems/minimum-depth-of-binary-tree/
+//    public int minDepth(TreeNode root) {
+//        if(root==null)
+//            return 0;
+//        int leftHeight=minDepth(root.left);
+//        int rightHeight=minDepth(root.right);
+//        return leftHeight==0 || rightHeight==0? 1+Math.max(leftHeight, rightHeight):1+Math.min(leftHeight, rightHeight);
+//
+//    }
+}
